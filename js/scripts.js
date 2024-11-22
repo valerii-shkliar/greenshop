29dbfda938bd1b7968e71bec0daf265012014cfd
Valerii Shkliar <walerik2848@gmail.com>
2024-11-22 21:58:41 +0200

    add scss-files

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+// $(document).ready(function () {
+//   $('.owl-carousel').owlCarousel();
+// });
+
+// $('.owl-carousel').owlCarousel({
+//   autoplay: true,
+//   autoplayTimeout: 6500,
+//   autoplaySpeed: 1000,
+//   loop: true,
+//   responsive: {
+//     0: {
+//       items: 1,
+//     },
+//     1100: {
+//       items: 4,
+//     },
+//   },
+// });
+
+// var menuToggle = document.querySelector('[data-js="menu-toggle"]');
+
+// Remove this setInterval to trigger the open/close manually through the UI
+// var interval = setInterval(function () {
+//   menuToggle.click();
+// }, 2000);
+
+// Clear the interval on any click
+// document.body.addEventListener('click', function () {
+//   clearInterval(interval);
+// });
+
+// menuToggle.addEventListener('click', function () {
+//   document.body.classList.toggle('panel-open');
+//   menuToggle.classList.toggle('open');
+// });
+
+// var closePanel = document.querySelector('[data-js="hidden-panel-close"]');
+
+// closePanel.addEventListener('click', function () {
+//   document.body.classList.remove('panel-open');
+//   menuToggle.classList.remove('open');
+// });
